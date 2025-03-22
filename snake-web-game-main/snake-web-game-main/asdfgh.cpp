8f424f70ce876767d56f4fb9992b23fbe9a20d38
AKSHAT PANKAJ VISHWAKARMA <akshatvishwakarma259@gmail.com>
2025-03-22 16:59:33 +0530

    Add files via upload

---
+++ snake-web-game-main/snake-web-game-main/asdfgh.cpp
@@ -0,0 +1,24 @@
+class sloution {
+    public;
+    void dfs
+}
+  body{
+            
+            padding:0px;
+            display:flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center; 
+            background-repeat: no-repeat;
+            background-position: center;
+            background-size: cover;
+            margin: 0;  /* Removes default margin */
+            height: 100vh;  
+            background-image: url("145a450800-d041-45ad-c37c-3b4e12a944.jpg")
+            
+            
+         }
+        canvas{
+            box-shadow: rgb(8, 0, 0) 20px 10px 50px;
+        }
+           
